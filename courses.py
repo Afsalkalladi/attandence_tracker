239f741839516d15bd12c4c4c08b3eb10b78ad04
Afsalkalladi <76440747+Afsalkalladi@users.noreply.github.com.com>
2024-09-01 22:02:54 +0530

    added one more class for course

--- courses.py
+++ courses.py
@@ -4,7 +4,6 @@ timetable = {}
 for i in range(int(5)):
     day = input("Enter day: ")
     for j in range(int(6)):
-        course = input("Enter course for "j" hour: ")
         timetable = {
             day
         }
